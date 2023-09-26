371a94fa0b4fcde0c1328f8dc2527375f2eba3b3
caiovictors <cvsarmento@hotmail.com>
2023-09-25 22:42:25 -0300

    Finishing .env and oneSignal configs

--- App.tsx
+++ App.tsx
@@ -1,17 +1,29 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Platform, StatusBar } from "react-native";
+import OneSignal from "react-native-onesignal";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Routes } from './src/routes';
+import { Routes } from "./src/routes";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+import { CartContextProvider } from "./src/contexts/CartContext";
+import { ONE_SIGNAL_ID } from "@env";
+
+const oneSignalAppId = Platform.OS === "android" ? ONE_SIGNAL_ID : "";
+OneSignal.setAppId(oneSignalAppId!);
+
+OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+  console.log("Prompt response:", response);
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-
   return (
     <NativeBaseProvider theme={THEME}>
       <StatusBar
---
+++ src/@types/env.d.ts
@@ -0,0 +1,3 @@
+declare module "@env" {
+  export const ONE_SIGNAL_ID: string;
+}
